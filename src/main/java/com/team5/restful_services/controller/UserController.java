4a5733174e8a1181050abe835caed59610a7e6cb
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2024-06-12 01:33:41 +0800

    amend UserController
    Signed-off-by: Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>

--- src/main/java/com/team5/restful_services/controller/UserController.java
+++ src/main/java/com/team5/restful_services/controller/UserController.java
@@ -1,7 +1,28 @@
 package com.team5.restful_services.controller;
 
+import com.team5.restful_services.model.User;
+import com.team5.restful_services.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
