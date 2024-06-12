3409a25fa9bdaefb3efd8949c885250b0839340a
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2024-06-12 17:01:33 +0800

    add CrossOrigin
    Signed-off-by: Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>

--- src/main/java/com/team5/restful_services/controller/UserController.java
+++ src/main/java/com/team5/restful_services/controller/UserController.java
@@ -3,14 +3,12 @@ package com.team5.restful_services.controller;
 import com.team5.restful_services.model.User;
 import com.team5.restful_services.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
